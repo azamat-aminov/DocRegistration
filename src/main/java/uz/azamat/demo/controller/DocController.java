93ae933bb567dee9a1345a0e1bce8cdcd32ce5c7
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-07 16:44:46 +0500

    in the DocController class Post request created

--- src/main/java/uz/azamat/demo/controller/DocController.java
+++ src/main/java/uz/azamat/demo/controller/DocController.java
@@ -2,6 +2,13 @@ package uz.azamat.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import uz.azamat.demo.model.RegistrationForm;
+
+import java.io.File;
+import java.util.Date;
 
 @Controller
 public class DocController {
@@ -12,6 +19,12 @@ public class DocController {
 
     @GetMapping("/main")
     public String mainPage() {
+        return "main";
+    }
+
+    @PostMapping("/someUrl")
+    public String someMethod(RegistrationForm registrationForm) {
+        System.out.println(registrationForm.getRegisterNumber());
         return "main";
     }
 }
