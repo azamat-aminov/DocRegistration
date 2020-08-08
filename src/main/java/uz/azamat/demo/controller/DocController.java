d37b28b79e69ae0b740b5c1734e4693f2c8764d0
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-08 11:48:23 +0500

    unnecessary imports removed from controller and modified main.html file

--- src/main/java/uz/azamat/demo/controller/DocController.java
+++ src/main/java/uz/azamat/demo/controller/DocController.java
@@ -3,12 +3,7 @@ package uz.azamat.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import uz.azamat.demo.model.RegistrationForm;
-
-import java.io.File;
-import java.util.Date;
 
 @Controller
 public class DocController {
