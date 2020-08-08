2c33af31e79f218b3b0bc8b43cf04bfa15341a66
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-08 18:48:59 +0500

    created template ArrayLists for options

--- src/main/java/uz/azamat/demo/controller/DocController.java
+++ src/main/java/uz/azamat/demo/controller/DocController.java
@@ -1,9 +1,15 @@
 package uz.azamat.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import uz.azamat.demo.model.CorrespondentType;
+import uz.azamat.demo.model.DeliveryType;
 import uz.azamat.demo.model.RegistrationForm;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class DocController {
@@ -13,7 +19,19 @@ public class DocController {
     }
 
     @GetMapping("/main")
-    public String mainPage() {
+    public String mainPage(Model model) {
+        List<DeliveryType> deliveryTypes = new ArrayList<>();
+        deliveryTypes.add(new DeliveryType(1, "Курьер"));
+        deliveryTypes.add(new DeliveryType(2, "Email"));
+        deliveryTypes.add(new DeliveryType(3, "Телефонограмма"));
+        model.addAttribute("forms", deliveryTypes);
+
+        List<CorrespondentType> correspondentTypes = new ArrayList<>();
+        correspondentTypes.add(new CorrespondentType(1, "ЦБ"));
+        correspondentTypes.add(new CorrespondentType(2, "ГНИ"));
+        correspondentTypes.add(new CorrespondentType(3, "ТСЖ"));
+        model.addAttribute("correspondents", correspondentTypes);
+
         return "main";
     }
 
