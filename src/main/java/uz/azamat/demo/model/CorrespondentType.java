ce8d6b5986f718cf0514c4c39004b78b40a76fb5
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-08 18:44:50 +0500

    CorrespondentType and DeliveryType model created

---
+++ src/main/java/uz/azamat/demo/model/CorrespondentType.java
@@ -0,0 +1,27 @@
+package uz.azamat.demo.model;
+
+public class CorrespondentType {
+    private int id;
+    private String name;
+
+    public CorrespondentType(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
