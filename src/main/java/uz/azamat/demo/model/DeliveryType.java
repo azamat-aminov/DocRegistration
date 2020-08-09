464652fa4f61f314cc202317dcfe1635e21d9e40
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-09 19:51:41 +0500

    lombok added to project

--- src/main/java/uz/azamat/demo/model/DeliveryType.java
+++ src/main/java/uz/azamat/demo/model/DeliveryType.java
@@ -1,30 +1,9 @@
 package uz.azamat.demo.model;
 
+import lombok.Data;
+
+@Data
 public class DeliveryType {
     private int id;
     private String name;
-
-    public DeliveryType() {
-    }
-
-    public DeliveryType(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/uz/azamat/demo/model/RegistrationForm.java
+++ src/main/java/uz/azamat/demo/model/RegistrationForm.java
@@ -1,7 +1,8 @@
 package uz.azamat.demo.model;
 
+import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
-
+@Data
 public class RegistrationForm {
     private String registerNumber;
     private String registerDate;
@@ -15,112 +16,4 @@ public class RegistrationForm {
     private Boolean access;
     private Boolean control;
     private MultipartFile file;
-
-    public String getRegisterNumber() {
-        return registerNumber;
-    }
-
-    public RegistrationForm setRegisterNumber(String registerNumber) {
-        this.registerNumber = registerNumber;
-        return this;
-    }
-
-    public String getRegisterDate() {
-        return registerDate;
-    }
-
-    public RegistrationForm setRegisterDate(String registerDate) {
-        this.registerDate = registerDate;
-        return this;
-    }
-
-    public String getLeaveRegisterNumber() {
-        return leaveRegisterNumber;
-    }
-
-    public RegistrationForm setLeaveRegisterNumber(String leaveRegisterNumber) {
-        this.leaveRegisterNumber = leaveRegisterNumber;
-        return this;
-    }
-
-    public String getLeaveDate() {
-        return leaveDate;
-    }
-
-    public RegistrationForm setLeaveDate(String leaveDate) {
-        this.leaveDate = leaveDate;
-        return this;
-    }
-
-    public String getFormDelivery() {
-        return formDelivery;
-    }
-
-    public RegistrationForm setFormDelivery(String formDelivery) {
-        this.formDelivery = formDelivery;
-        return this;
-    }
-
-    public String getCorrespondent() {
-        return correspondent;
-    }
-
-    public RegistrationForm setCorrespondent(String correspondent) {
-        this.correspondent = correspondent;
-        return this;
-    }
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public RegistrationForm setTopic(String topic) {
-        this.topic = topic;
-        return this;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public RegistrationForm setDescription(String description) {
-        this.description = description;
-        return this;
-    }
-
-    public String getExecutionPeriod() {
-        return executionPeriod;
-    }
-
-    public RegistrationForm setExecutionPeriod(String executionPeriod) {
-        this.executionPeriod = executionPeriod;
-        return this;
-    }
-
-    public Boolean getAccess() {
-        return access;
-    }
-
-    public RegistrationForm setAccess(Boolean access) {
-        this.access = access;
-        return this;
-    }
-
-    public Boolean getControl() {
-        return control;
-    }
-
-    public RegistrationForm setControl(Boolean control) {
-        this.control = control;
-        return this;
-    }
-
-    public MultipartFile getFile() {
-        return file;
-    }
-
-    public RegistrationForm setFile(MultipartFile file) {
-        this.file = file;
-        return this;
-    }
 }
