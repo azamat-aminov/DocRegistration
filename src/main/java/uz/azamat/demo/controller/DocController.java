ef2c3b3f3808617735ca855f2418bb601a9f5771
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-09 19:46:41 +0500

    removed from controller temp ArrayLists and get from service getAllDelivery and getAllCorrespondent

--- src/main/java/uz/azamat/demo/controller/DocController.java
+++ src/main/java/uz/azamat/demo/controller/DocController.java
@@ -4,15 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import uz.azamat.demo.controller.service.CorrespondentTypeService;
+import uz.azamat.demo.controller.service.DeliveryService;
 import uz.azamat.demo.model.CorrespondentType;
 import uz.azamat.demo.model.DeliveryType;
 import uz.azamat.demo.model.RegistrationForm;
 
-import java.util.ArrayList;
+import javax.annotation.Resource;
 import java.util.List;
 
 @Controller
 public class DocController {
+    @Resource
+    DeliveryService deliveryService;
+    @Resource
+    CorrespondentTypeService correspondentTypeService;
     @GetMapping()
     public String display() {
         return "welcome";
@@ -20,18 +26,11 @@ public class DocController {
 
     @GetMapping("/main")
     public String mainPage(Model model) {
-        List<DeliveryType> deliveryTypes = new ArrayList<>();
-        deliveryTypes.add(new DeliveryType(1, "Курьер"));
-        deliveryTypes.add(new DeliveryType(2, "Email"));
-        deliveryTypes.add(new DeliveryType(3, "Телефонограмма"));
-        model.addAttribute("forms", deliveryTypes);
+        List<DeliveryType> allDelivery = deliveryService.getAllDelivery();
+        model.addAttribute("forms", allDelivery);
 
-        List<CorrespondentType> correspondentTypes = new ArrayList<>();
-        correspondentTypes.add(new CorrespondentType(1, "ЦБ"));
-        correspondentTypes.add(new CorrespondentType(2, "ГНИ"));
-        correspondentTypes.add(new CorrespondentType(3, "ТСЖ"));
-        model.addAttribute("correspondents", correspondentTypes);
-
+        List<CorrespondentType> allCorrespondent = correspondentTypeService.getAllCorrespondent();
+        model.addAttribute("correspondents", allCorrespondent);
         return "main";
     }
 
